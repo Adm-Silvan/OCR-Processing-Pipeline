e99a040d4dcf1661beefb702e91e513fa67df27f
Adm-Silvan <silvan.adm@hotmail.com>
2025-09-29 00:05:29 +0200

    remove duplicates

--- Chunker/install_jina.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer, AutoModel
-model_name = "jinaai/jina-embeddings-v3"
-tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
